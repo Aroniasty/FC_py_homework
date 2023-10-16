cf3db650e44028340b246ccf1320911fbc8a9a42
Aroniasty <aroniasty@gmail.com>
2023-10-16 17:03:49 +0200

    testowanie różnych rozwiązań

--- Python_02/Program_do_obslugi_ladowarki_paczek.py
+++ Python_02/Program_do_obslugi_ladowarki_paczek.py
@@ -14,6 +14,16 @@
 
 print("Witaj w programie do obsługi ładowarki paczek.\n")
 
+wagi_paczek = [] #lista przechowuje zmienne/wartosci
+print (wagi_paczek)
+wagi_paczek.append(3)
+wagi_paczek.append(4)
+wagi_paczek.append(5)
+wagi_paczek.append(6)
+print(wagi_paczek)
+print(wagi_paczek[0])
+print(min(wagi_paczek))
+
 koniec_programu = False
 
 limit_wagi_paczki = 20 # <=20kg
@@ -49,15 +59,23 @@ while not koniec_programu:
             if not int(waga_elementu) in range (1,11):
                 print("Nieprawidłowa waga elmentu.")
                 continue
-            waga_paczki += int(waga_elementu)
-            print(f"Całkowita waga paczki: {waga_paczki} kg")
+            if waga_paczki + int(waga_elementu) > 20:
+                print("Osiągnięto limit wagi paczki")
+                wagi_paczek.append(waga_paczki)
+                waga_paczki = waga_elementu
+                print(wagi_paczek)
+                break
+            else:
+                waga_paczki += int(waga_elementu)
+                print(f"Całkowita waga paczki: {waga_paczki} kg")
+
         else: #if int(waga_paczki) == 20:
             print("Osiągnięto limit wagi paczki")
             break
 # program wychodzi do opcji i nie można już dodawać elementów do paczki.
 
-    elif int(wybor_menu) == 2:
-        koniec_programu = True
+    # elif int(wybor_menu) == 2:
+    #     koniec_programu = True
 print("Program zakończył działanie.\nPodsumowanie: ")
 #print(f" - Liczba wysłanych paczek: {licznik_paczek} szt.")
 print(f" - Całkowita waga paczek: {waga_paczki} kg")
