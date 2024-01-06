0cbf9a4786ad2a7e7895ef826a54dfb7d8ad292d
Aroniasty <aroniasty@gmail.com>
2024-01-06 13:20:03 +0100

    update

--- Project/csv_reader.py
+++ Project/csv_reader.py
@@ -1,6 +1,20 @@
 import csv
+from project import Data
 
-with open("cost_of_living_us.csv") as file:
-    data = csv.reader(file)
-    for line in data:
-        print(line)
+with open("cost_of_living_us.csv", "r", newline="", encoding="") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    # for line in csv_reader:
+    #     print(line)
+    for row in csv_reader:
+        data_row = Data(
+            state=row[1],
+            areaname=row[3],
+            total_cost=float(row[13]),
+            median_family_income=float(row[14])
+        )
+        db.session.add(data_row)
+
+db.session.commit()
+
+session.close()
--- Project/project.py
+++ Project/project.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///project.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -24,15 +25,25 @@ def about():
 
 
 class Data(db.Model):
+    __tablename__ = "cost_of_living_us"
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String, nullable=False)
     areaname = db.Column(db.String, nullable=False)
-    total_cost = db.Column(db.Integer, nullable=False, default=0)
-    median_family_income = db.Column(db.Integer, nullable=False, default=0)
+    total_cost = db.Column(db.Float, nullable=False, default=0)
+    median_family_income = db.Column(db.Float, nullable=False, default=0)
+
+# engine = create_engine('sqlite:///project.db')
+# db.create_all()
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-# with app.app_context():
-    # db.create_all() /// odkomentowac jak bedzie gotowa klasa
+with app.app_context():
+    db.create_all()
+    test = Data(state="SS", areaname="AA", total_cost="1111.11", median_family_income="2222.22")
+    db.session.add_all([test])
+    db.session.commit()
     # kod do obsługi tabeli
 
 if __name__ == "__main__":
