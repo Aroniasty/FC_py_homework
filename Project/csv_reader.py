b7db926e086de366d84959b806ca97843b3d6013
Aroniasty <aroniasty@gmail.com>
2024-01-26 14:42:28 +0100

    Update

--- Project/csv_reader.py
+++ Project/csv_reader.py
@@ -3,7 +3,6 @@ import csv
 def read_data_from_csv():
     data = []
     with open("cost_of_living_us.csv", "r") as csv_file:
-    # with open("csv_test.csv", "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         next(csv_reader)
         for line in csv_reader:
